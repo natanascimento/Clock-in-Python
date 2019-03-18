18f507f07bced42ee3aa486db923e844c3e1a2b0
Natan Nascimento <natanascimentom@outlook.com>
2019-03-18 17:22:43 -0300

    Construindo a interface Tkinter

--- clock.py
+++
@@ -1,3 +0,0 @@
-from tkinter import *
-from time import strftime
-
---
+++ main/clock.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from time import strftime
+
+
+root = Tk()
+
+main_structure = Label(root, text = "HELLO")
+main_structure.pack()
+
+root.title("Clock")
+root.mainloop()
