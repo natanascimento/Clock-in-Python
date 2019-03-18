adca7434bd3a59a640fa3b4ec20f7c771e272994
Natan Nascimento <natanascimentom@outlook.com>
2019-03-18 17:36:12 -0300

    Make clock function

--- main/clock.py
+++ main/clock.py
@@ -1,11 +1,24 @@
 from tkinter import *
 from time import strftime
 
+#For make the clock function
+def clock():
+    def clock_main():
+        main_structure['text'] = strftime("%H:%M:%S")
+    def counter():
+        clock_main()
+        root.after(1000,counter)
+    counter()
 
+#Interface structure
 root = Tk()
-
-main_structure = Label(root, text = "HELLO")
+#Structure for plot the clock on GUI
+main_structure = Label(root, text = "--:--:--")
 main_structure.pack()
 
+#For enable the clock 
+clock()
+
 root.title("Clock")
+#If haven't "mainloop", in this code, the program stop in the clock function, and not would tell the seconds
 root.mainloop()
